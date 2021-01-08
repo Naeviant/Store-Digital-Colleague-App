19ec79764f1e2289d786375d4a48389244a8a641
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-08 15:10:36 +0000

    redirect after login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from "react-router-dom";
 import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
@@ -20,7 +21,8 @@ class Login extends React.Component {
 		super(props);
 		this.state = {
 			username: '',
-			password: ''
+			password: '',
+			redirect: ''
 		};
 	}
 
@@ -32,6 +34,7 @@ class Login extends React.Component {
 		axios.post('/authenticate', this.state).then(async (token) => {
 			const user = await axios.get('/user/' + this.state.username, { headers: { Authorization: token.data.data } });
 			this.props.store(token.data.data, user.data.data);
+			this.setState({ ...this.state, redirect: '/' });
 		}, (error) => {
 			if (error.response.status) {
 				console.log('Invalid Credentials')
@@ -43,36 +46,41 @@ class Login extends React.Component {
 
 	render() {
 		const { classes } = this.props;
-		return (
-			<Grid
-			  container
-			  spacing={0}
-			  direction="column"
-			  alignItems="center"
-			  justify="center"
-			  style={{ minHeight: '100vh' }}
-			>
-			  	<Grid item>
-				    <Card>
-						<CardContent>
-							<Grid item>
-								<Typography align="center" variant="h3" paragraph>Welcome</Typography>
-								<Typography align="center" variant="body1" paragraph>Please enter your credentials.</Typography>
-							</Grid>
-							<Grid item justify="center">
-								<TextField id="username" placeholder="Username" variant="outlined" type="text" onChange={this.onText} className={classes.loginInput} />
-							</Grid>
-							<Grid item justify="center">
-								<TextField id="password" placeholder="Password" variant="outlined" type="password" onChange={this.onText} className={classes.loginInput} />
-							</Grid>
-							<Grid item justify="center">
-								<Button variant="contained" size="large" color="primary" onClick={this.onSubmit} className={classes.loginInput}>Login</Button>
-							</Grid>
-						</CardContent>
-					</Card>
-			  	</Grid>   
-			</Grid>
-		)
+		if (this.state.redirect) {
+			return <Redirect to={this.state.redirect} />;
+		}
+		else {
+			return (
+				<Grid
+				  container
+				  spacing={0}
+				  direction="column"
+				  alignItems="center"
+				  justify="center"
+				  style={{ minHeight: '100vh' }}
+				>
+				  	<Grid item>
+					    <Card>
+							<CardContent>
+								<Grid item>
+									<Typography align="center" variant="h3" paragraph>Welcome</Typography>
+									<Typography align="center" variant="body1" paragraph>Please enter your credentials.</Typography>
+								</Grid>
+								<Grid item justify="center">
+									<TextField id="username" placeholder="Username" variant="outlined" type="text" onChange={this.onText} className={classes.loginInput} />
+								</Grid>
+								<Grid item justify="center">
+									<TextField id="password" placeholder="Password" variant="outlined" type="password" onChange={this.onText} className={classes.loginInput} />
+								</Grid>
+								<Grid item justify="center">
+									<Button variant="contained" size="large" color="primary" onClick={this.onSubmit} className={classes.loginInput}>Login</Button>
+								</Grid>
+							</CardContent>
+						</Card>
+				  	</Grid>   
+				</Grid>
+			)
+		}
 	}
 }
 
