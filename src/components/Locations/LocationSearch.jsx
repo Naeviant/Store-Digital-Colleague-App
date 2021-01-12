ed5e52f6e63b9701ee55ce3fec0cd95927607cad
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-12 19:25:25 +0000

    added location search functionality

--- src/components/Locations/LocationSearch.jsx
+++ src/components/Locations/LocationSearch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
@@ -11,17 +12,54 @@ const useStyles = theme => ({
 });
 
 class LocationSearch extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			location: '',
+			redirect: ''
+		}
+	}
+
+	onChange = (e) => {
+		const re = /^[0-9-]+$/;
+		const value = e.target.value;
+		if (value === '' || re.test(value)) this.setState({ ...this.state, location: e.target.value });
+	}
+
+	onKeypress = (e) => {
+		if (e.key === 'Enter') {
+			this.onClick();
+		}
+	}
+
+	onClick = () => {
+		const parts = this.state.location.split('-');
+		if (parts[0] && parts[1]) {
+			this.setState({ ...this.state, redirect: '/locations/' + parts[0] + '/' + parts[1] });
+		} else if (parts[0]) {
+			this.setState({ ...this.state, redirect: '/locations/' + parts[0] });
+		}
+	}
+
 	render() {
+		if (this.state.redirect) {
+			this.props.history.push(this.state.redirect);
+			this.setState({ ...this.state, redirect: '' });
+		}
 		return (
 			<Box p={1}>
 				<Paper>
 					<Box pl={2}>
 					  	<InputBase
 					        placeholder="Enter Location"
-					        style={{ width: 'calc(100% - 56px)' }}
+							style={{ width: 'calc(100% - 56px)' }}
+							onChange={this.onChange}
+							inputProps={{ onKeyDown: this.onKeypress }}
+							value={this.state.location}
 					    />
 					    <IconButton 
-					    	color="primary"
+							color="primary"
+							onClick={this.onClick}
 					    >
 					        <SearchIcon />
 					    </IconButton>
@@ -32,4 +70,4 @@ class LocationSearch extends React.Component {
 	}
 }
 
-export default withStyles(useStyles)(LocationSearch);
+export default withRouter(withStyles(useStyles)(LocationSearch));
