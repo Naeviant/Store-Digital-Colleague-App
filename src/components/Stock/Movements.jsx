8f7ce1f6c3ece92478e95ed7baec3209622ee4b8
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-15 17:44:34 +0000

    added functionality to movements screen

--- src/components/Stock/Movements.jsx
+++ src/components/Stock/Movements.jsx
@@ -11,13 +11,17 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import SearchIcon from '@material-ui/icons/Search';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
+import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
 const useStyles = theme => ({
 	backdrop: {
@@ -41,6 +45,9 @@ class Movements extends React.Component {
         this.state = {
             ean: '',
             product: {},
+            movements: [],
+            quantity: 1,
+            type: '',
             loading: false
         };
     }
@@ -61,7 +68,7 @@ class Movements extends React.Component {
             this.setState({ ...this.state, product: product.data.data, loading: false });
 		}, (error) => {
 			switch(error.response.data.description) {
-                case 'Invalid EAN Provided':
+                case 'Product Not Found':
                     this.setState({ ...this.state, loading: false });
                     this.props.showBanner('Invalid EAN Provided', 'warning');
 					break;
@@ -70,6 +77,44 @@ class Movements extends React.Component {
                     this.props.showBanner('Something Went Wrong', 'error');
             }
 		});
+    }
+
+    quantityChange = (e) => {
+        if (!isNaN(Number(e.target.value)) && Number(e.target.value) > 0) {
+            this.setState({ ...this.state, quantity: Number(e.target.value) });
+        }
+    }
+
+    typeChange = (e) => {
+        this.setState({ ...this.state, type: e.target.value });
+    }
+    
+    add = (action) => {
+        const movements = this.state.movements;
+        movements.push({
+            product: this.state.product,
+            quantity: this.state.quantity,
+            type: this.state.type,
+            action: action
+        });
+        this.setState({ ...this.state, product: {}, movements: movements, quantity: 1, type: '' });
+    }
+
+    delete = (index) => {
+        const movements = this.state.movements;
+        movements.splice(index, 1);
+        this.setState({ ...this.state, movements: movements });
+    }
+
+    submit = async () => {
+        for (const movement of this.state.movements) {
+            await axios.patch('/product/quantity/' + this.props.apiUser.site.code + '/' + movement.product.ean, {
+                method: movement.action,
+                quantity: movement.quantity
+            }, { headers: { Authorization: this.props.apiToken } });
+        }
+        this.setState({ ...this.state, movements: [] });
+        this.props.showBanner('Stock Movements Recorded', 'success');
     }
 
 	render() {
@@ -94,29 +139,83 @@ class Movements extends React.Component {
                             />
                             <IconButton 
                                 color="primary"
-                                onClick={this.props.search}
+                                onClick={this.search}
                             >
                                 <SearchIcon />
                             </IconButton>
                         </Box>
                     </Paper>
                 </Box>
-                <Box p={1}>
-                    <Card>
-                        <CardContent className={classes.cardContent}>
-                            <Divider />
-                            <Table>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell>
-                                            <Button variant="contained" color="primary" onClick={this.submit}>Submit</Button>
-                                        </TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </CardContent>
-                    </Card>
-                </Box>
+                {
+                    this.state.product.ean && 
+                    <Box p={1}>
+                        <Card>
+                            <CardContent className={classes.cardContent}>
+                                <InputBase
+                                    placeholder="Quantity"
+                                    style={{ width: 'calc(50% - 96px)' }}
+                                    onChange={this.quantityChange}
+                                    value={this.state.quantity}
+                                />
+                                <Select
+                                    style={{ width: 'calc(50% - 96px)' }}
+                                    onChange={this.typeChange}
+                                    value={this.state.type}
+                                >
+                                    <MenuItem value="Known Theft">Known Theft</MenuItem>
+                                    <MenuItem value="Store Own Use">Store Own Use</MenuItem>
+                                    <MenuItem value="Sample">Sample</MenuItem>
+                                    <MenuItem value="Damage">Damage</MenuItem>
+                                    <MenuItem value="Expired">Expired</MenuItem>
+                                </Select>
+                                <Button variant="contained" color="primary" onClick={() => { this.add('decrement') }} style={{ marginLeft: 20 }}>Add</Button>
+                                <Button variant="contained" color="secondary" onClick={() => { this.add('increment') }} style={{ marginLeft: 5 }}>Reverse</Button>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                }
+                {
+                    this.state.movements.length > 0 &&
+                    <Box p={1}>
+                        <Card>
+                            <CardContent className={classes.cardContent}>
+                                <Divider />
+                                <Table>
+                                    <TableBody>
+                                        {
+                                            this.state.movements.map((row, index) => (
+                                                <TableRow key={index}>
+                                                    <TableCell className="tableCell">
+                                                        <ListItemText primary={row.product.name} secondary={row.product.ean} />
+                                                    </TableCell>
+                                                    <TableCell className="tableCell">{row.quantity}</TableCell>
+                                                    <TableCell className="tableCell">{row.type}{row.action === 'increment' && ' (Reversal)'}</TableCell>
+                                                    <TableCell className="tableCell">
+                                                        <IconButton 
+                                                            color="primary"
+                                                            onClick={() => { this.delete(index) }}
+
+                                                        >
+                                                            <DeleteForeverIcon />
+                                                        </IconButton>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))
+                                        }
+                                        <TableRow>
+                                            <TableCell></TableCell>
+                                            <TableCell></TableCell>
+                                            <TableCell></TableCell>
+                                            <TableCell>
+                                                <Button variant="contained" color="primary" onClick={this.submit}>Submit</Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                }
             </>
 		)
 	}
