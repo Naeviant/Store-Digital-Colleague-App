4ac82978e53ea7b0bb3783d3ecab4b5a508d84c3
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-19 20:32:16 +0000

    added stock check popup to products screen

--- src/components/Products/ProductButtons.jsx
+++ src/components/Products/ProductButtons.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductCheckStock from './ProductCheckStock';
 import ProductDeliveries from './ProductDeliveries';
 import ProductModules from './ProductModules';
 import Box from '@material-ui/core/Box';
@@ -23,7 +24,7 @@ class ProductButtons extends React.Component {
 				<Box p={1}>
 					<Grid container spacing={3}>
 						<Grid item xs={12} sm={12} md={4}>
-							<Button variant="contained" color="primary" fullWidth size="large">Search Other Sites</Button>
+							<Button variant="contained" color="primary" fullWidth size="large" onClick={() => { this.setMenu('stock'); }}>Search Other Sites</Button>
 						</Grid>
 						<Grid item xs={12} sm={6} md={4}>
 							<Button variant="contained" color="primary" fullWidth size="large" onClick={() => { this.setMenu('deliveries'); }}>Upcoming Deliveries</Button>
@@ -33,6 +34,7 @@ class ProductButtons extends React.Component {
 						</Grid>
 					</Grid>
 				</Box>
+				<ProductCheckStock open={this.state.menu === 'stock'} close={() => { this.setMenu(null); }} ean={this.props.ean} />
 				<ProductDeliveries open={this.state.menu === 'deliveries'} close={() => { this.setMenu(null); }} deliveries={this.props.deliveries} ean={this.props.ean} />
 				<ProductModules open={this.state.menu === 'modules'} close={() => { this.setMenu(null); }} modules={this.props.modules} />
 			</>
