728de6e18e892ff1a455d14d10fe5a76e4e9d6c4
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2021-01-22 15:20:09 +0000

    added reviews tab to products screen

--- src/components/Products/ProductInfo.jsx
+++ src/components/Products/ProductInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TabPanel from '../common/TabPanel';
 import AppBar from "@material-ui/core/AppBar";
 import Box from '@material-ui/core/Box';
+import StarIcon from '@material-ui/icons/Star';
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import Table from '@material-ui/core/Table';
@@ -35,7 +36,7 @@ class ProductButtons extends React.Component {
 			        >
 			        	<Tab label="Description" />
 			        	<Tab label="Info" />
-			        	<Tab disabled label="Reviews" />
+			        	<Tab label="Reviews" />
 			        </Tabs>
 			        <TabPanel value={this.state.tab} index={0}>
 						<Box p={1}>
@@ -51,7 +52,7 @@ class ProductButtons extends React.Component {
 							{
 								this.props.product.info.length === 0
 								?
-								<em>No Information Provided</em>
+								<Typography variant="body1"><em>No Reviews Provided</em></Typography>
 								:
 								<Table>
 									<TableBody>
@@ -68,7 +69,37 @@ class ProductButtons extends React.Component {
 							}
 						</Box>
 					</TabPanel>
-			        <TabPanel value={this.state.tab} index={2}></TabPanel>
+			        <TabPanel value={this.state.tab} index={2}>
+						<Box p={1}>
+							{
+								this.props.reviews.length === 0
+								?
+								<Typography variant="body1"><em>No Reviews Provided</em></Typography>
+								:
+								this.props.reviews.map(row => (
+									<Box p={1}>
+										<Typography variant="h6">
+											{
+												row.customer.firstName 
+												+ ' ' + row.customer.lastName
+												+ ' (' + row.timestamp.split('-')[2].split('T')[0] 
+												+ '/' + row.timestamp.split('-')[1]
+												+ '/' + row.timestamp.split('-')[0] + ')'
+											}
+										</Typography>
+										<Box>
+											{ row.rating > 0 ? <StarIcon fontSize='small' style={{ color: '#ffd54f' }} /> : <StarIcon fontSize='small' style={{ color: 'grey' }} /> }
+											{ row.rating > 1 ? <StarIcon fontSize='small' style={{ color: '#ffd54f' }} /> : <StarIcon fontSize='small' style={{ color: 'grey' }} /> }
+											{ row.rating > 2 ? <StarIcon fontSize='small' style={{ color: '#ffd54f' }} /> : <StarIcon fontSize='small' style={{ color: 'grey' }} /> }
+											{ row.rating > 3 ? <StarIcon fontSize='small' style={{ color: '#ffd54f' }} /> : <StarIcon fontSize='small' style={{ color: 'grey' }} /> }
+											{ row.rating > 4 ? <StarIcon fontSize='small' style={{ color: '#ffd54f' }} /> : <StarIcon fontSize='small' style={{ color: 'grey' }} /> }
+										</Box>
+										<Typography variant="body1">{row.review}</Typography>
+									</Box>
+								))
+							}
+						</Box>
+					</TabPanel>
 			    </AppBar>
 			</Box>
 		);
